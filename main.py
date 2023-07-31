1e455c9d75655b650f650b3b2e6162e3b02eb5d8
akaiaui99 <141066915+akaiaui99@users.noreply.github.com>
2023-07-31 18:54:11 +0000

    main.py

--- main.py
+++ main.py
@@ -6,24 +6,19 @@ rand_links = [
     "https://netactuaia7822.com/?opaioa89aHJhj"
 ]
 
-last_link_index = -1
-
 def get_random_link():
-    global last_link_index
-    while True:
-        rand_link_index = random.randint(0, len(rand_links) - 1)
-        if rand_link_index != last_link_index:
-            break
-    last_link_index = rand_link_index
-    return rand_links[rand_link_index]
+    return random.choice(rand_links)
 
 def main():
     st.set_page_config(page_title="Redirect Page", layout="wide")
 
     rand_link = get_random_link()
 
-    # Redirect the user
-    st.markdown(f'<meta http-equiv="refresh" content="0;URL=\'{rand_link}\'"/>', unsafe_allow_html=True)
+    # Create a button to trigger the redirect
+    if st.button("Visit Random Link"):
+        js_code = f"window.location.href = '{rand_link}';"
+        html_button = f'<button onclick="{js_code}">Visit Random Link</button>'
+        st.markdown(html_button, unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
