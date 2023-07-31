2062a7346c952c38ae4ee8705e1901330fa5b943
akaiaui99 <141066915+akaiaui99@users.noreply.github.com>
2023-07-31 19:01:04 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,14 @@
-import streamlit as st
-import random
+from flask import Flask, render_template
 
-rand_links = [
-    "https://netflxkauiaoia.com/?oaia9298298kja",
-    "https://netactuaia7822.com/?opaioa89aHJhj"
-]
+app = Flask(__name__)
 
-def get_random_link():
-    return random.choice(rand_links)
+@app.route('/')
+def index():
+    return "Selamat datang di halaman utama!"
 
-def main():
-    st.set_page_config(page_title="Redirect Page", layout="wide")
+@app.route('/about')
+def about():
+    return "Ini adalah halaman tentang kami."
 
-    rand_link = get_random_link()
-
-    # Display the random link
-    st.write(f"Click the button below to open the random link:")
-    if st.button("Open Random Link"):
-        st.experimental_rerun()  # Rerun the app to show the link options
-
-    if st.button("Open in New Tab"):
-        st.experimental_rerun()  # Rerun the app to show the link options
-        st.markdown(f'<a href="{rand_link}" target="_blank">Link</a>', unsafe_allow_html=True)
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    app.run()
