f594e1480685c1d96b8c83b4aa2b33517e842599
akaiaui99 <141066915+akaiaui99@users.noreply.github.com>
2023-07-31 19:14:08 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,25 @@
+import streamlit as st
+import random
+
 rand_links = [
     "https://netflxkauiaoia.com/?oaia9298298kja",
     "https://netactuaia7822.com/?opaioa89aHJhj"
 ]
 
-last_link_index = -1
-
 def get_random_link():
-    global last_link_index
-    while True:
-        rand_link_index = random.randint(0, len(rand_links) - 1)
-        if rand_link_index != last_link_index:
-            break
-    last_link_index = rand_link_index
-    return rand_links[rand_link_index]
+    return random.choice(rand_links)
 
 def main():
     st.set_page_config(page_title="Redirect Page", layout="wide")
 
     rand_link = get_random_link()
 
-    # Redirect the user
-    st.markdown(f'<meta http-equiv="refresh" content="0;URL=\'{rand_link}\'"/>', unsafe_allow_html=True)
+    # Display the random link
+    st.write(f"Click the button below to open a new tab and visit the random link:")
+    if st.button("Open Random Link in New Tab"):
+        js_code = f"window.open('{rand_link}', '_blank');"
+        html_button = f'<button onclick="{js_code}">Open Random Link in New Tab</button>'
+        st.markdown(html_button, unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
